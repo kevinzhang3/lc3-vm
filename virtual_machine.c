5ff6db2e494c69cf601f59ea215371a500e10236
Kevin <kzhang366@gmail.com>
2024-08-07 12:10:44 -0600

    registers

--- virtual_machine.c
+++ virtual_machine.c
@@ -2,3 +2,19 @@
 
 #define MEMORY_MAX (1 << 16)
 uint16_t memory[MEMORY_MAX];
+
+/* registers for the LC-3 */
+enum
+{
+    R_R0 = 0,
+    R_R1,
+    R_R2,
+    R_R3,
+    R_R4,
+    R_R5,
+    R_R6,
+    R_R7,
+    R_PC,
+    R_COND,
+    R_COUNT
+};
