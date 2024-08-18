11b33f7ddca86973ff6f7fef05d347367b7d412a
Kevin <kzhang366@gmail.com>
2024-08-18 13:42:01 -0600

    update flags

--- virtual_machine.c
+++ virtual_machine.c
@@ -103,6 +103,23 @@ uint16_t sign_extend(uint16_t x, int bit_count)
     return x;
 }
 
+/* updating condition flags any time a register is written */
+void update_flags(uint16_t r)
+{
+    if (reg[r] == 0)
+    {
+        reg[R_COND] = FL_ZRO;
+    }
+    else if (reg[r] >> 15) // a 1 in the left-most bit indicates negative
+    {
+        reg[R_COND] = FL_NEG;
+    }
+    else
+    {
+        reg[R_COND] = FL_POS;
+    }
+}
+
 /* simulate processing instructions in main loop */
 int main(int argc, const char *argv[])
 {
