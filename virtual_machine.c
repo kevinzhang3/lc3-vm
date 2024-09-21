dd751bfbf5c6c038f42bde7ddfc4bb587f76532c
Kevin <kzhang366@gmail.com>
2024-09-21 02:37:15 -0600

    memory r/w

--- virtual_machine.c
+++ virtual_machine.c
@@ -145,6 +145,28 @@ void update_flags(uint16_t r)
     }
 }
 
+void mem_write(uint16_t address, uint16_t val)
+{
+    memory[address] = val;
+}
+
+uint16_t mem_read(uint16_t address)
+{
+    if (address == MR_KBSR)
+    {
+        if (check_key())
+        {
+            memory[MR_KBSR] = (1 << 15);
+            memory[MR_KBDR] = getchar();
+        }
+        else
+        {
+            memory[MR_KBSR] = 0;
+        }
+    }
+    return memory[address];
+}
+
 /* simulate processing instructions in main loop */
 int main(int argc, const char *argv[])
 {
